37efb5d19aa0438277ee9546f14e9ac0140b3e69
sasha281990 <sasha-filanovich@mail.ru>
2026-04-28 22:36:21 +0300

    first task

--- Hello.java
+++ Hello.java
@@ -1,6 +1,8 @@
 public class Hello {
 
     public static void main(String[] args) {
-        System.out.println("Hello Sasha");
+        String developer = "Filanovich";
+        System.out.println("Фамилия разработчика: " + developer);
+        System.out.println("Дата и время получения задания: 22.04.2026 19:46:00");
     }
 }
