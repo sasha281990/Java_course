79efca6803d9bd2c4d6204883f2cb4add5ad6a33
Филанович Александр Александрович <aleksandr.filanovich@service-it.by>
2026-05-28 21:34:22 +0300

    Task 6

--- src/Task4.java
+++ src/Task4.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class task4 {
+public class Task4 {
     public static void main(String[] args) {
         Scanner sc = new Scanner(System.in);
 
--- src/Task5.java
+++ src/Task5.java
@@ -1,6 +1,6 @@
 import java.util.Scanner;
 
-public class task5 {
+public class Task5 {
     public static void main(String[] args) {
         Scanner scanner = new Scanner(System.in);
 
