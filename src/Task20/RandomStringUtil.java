dab752543209dc7147b31de53206de80c545f014
Филанович Александр Александрович <aleksandr.filanovich@service-it.by>
2026-07-13 20:08:48 +0300

    Task 20

---
+++ src/Task20/RandomStringUtil.java
@@ -0,0 +1,16 @@
+package src.Task20;
+
+import java.util.Random;
+
+public class RandomStringUtil {
+    private static final String ALPHABET = "abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789";
+    private static final Random RANDOM = new Random();
+
+    public static String randomString(int length) {
+        StringBuilder sb = new StringBuilder();
+        for (int i = 0; i < length; i++) {
+            sb.append(ALPHABET.charAt(RANDOM.nextInt(ALPHABET.length())));
+        }
+        return sb.toString();
+    }
+}
